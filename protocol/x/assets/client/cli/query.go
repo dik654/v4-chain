96c26ef043335e76faab1e167463cecf063308d8
김대익 <33992354+dik654@users.noreply.github.com>
2024-03-25 15:31:09 +0900

    Update query.go

--- protocol/x/assets/client/cli/query.go
+++ protocol/x/assets/client/cli/query.go
@@ -12,10 +12,13 @@ import (
 func GetQueryCmd(queryRoute string) *cobra.Command {
 	// Group assets queries under a subcommand
 	cmd := &cobra.Command{
+		// types/keys.go
+		// ModuleName = "assets"
 		Use:                        types.ModuleName,
 		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
+		// 정상적인 cmd 매개변수(ex. --arg)인지 체크하는 메서드
 		RunE:                       client.ValidateCmd,
 	}
 

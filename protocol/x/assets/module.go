7e0dd6a95cd9de5cfa1506f9667f36e05be3c9f3
김대익 <33992354+dik654@users.noreply.github.com>
2024-03-25 15:07:46 +0900

    Update module.go

--- protocol/x/assets/module.go
+++ protocol/x/assets/module.go
@@ -28,6 +28,7 @@ var (
 	_ module.HasServices         = AppModule{}
 )
 
+// 기본 모듈
 // ----------------------------------------------------------------------------
 // AppModuleBasic
 // ----------------------------------------------------------------------------
@@ -42,20 +43,24 @@ func NewAppModuleBasic(cdc codec.BinaryCodec) AppModuleBasic {
 }
 
 // Name returns the assets module's name.
+// asset 모듈 이름을 리턴하는 함수
 func (AppModuleBasic) Name() string {
 	return types.ModuleName
 }
 
+// Amino codec 등록
 func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	types.RegisterCodec(cdc)
 }
 
 // RegisterInterfaces registers the module's interface types
+// interface 타입 등록
 func (a AppModuleBasic) RegisterInterfaces(reg cdctypes.InterfaceRegistry) {
 	types.RegisterInterfaces(reg)
 }
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
+// gRPC Gateway 등록
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 	if err != nil {
@@ -75,12 +80,14 @@ func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 
 // DefaultGenesis returns the assets module's default genesis state.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	// Genesis를 JSON으로 마샬링하여 리턴
 	return cdc.MustMarshalJSON(types.DefaultGenesis())
 }
 
 // ValidateGenesis performs genesis state validation for the assets module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
 	var genState types.GenesisState
+	// genesis를 언마샬링 후 유효성 검사
 	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
 		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
 	}
@@ -92,12 +99,14 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncod
 // ----------------------------------------------------------------------------
 
 // AppModule implements the AppModule interface for the assets module.
+// 기본 app module과 keeper를 가지고 있는 asset app module 구조체 선언
 type AppModule struct {
 	AppModuleBasic
 
 	keeper keeper.Keeper
 }
 
+// asset app module 생성 함수
 func NewAppModule(
 	cdc codec.Codec,
 	keeper keeper.Keeper,
@@ -116,6 +125,8 @@ func (am AppModule) IsOnePerModuleType() {}
 
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
+// TODO : GRPC query service가 뭔지
+// grpc query 서비스 등록
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
@@ -123,17 +134,24 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 
 // InitGenesis performs the assets module's genesis initialization It returns
 // no validator updates.
+
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) {
 	var genState types.GenesisState
 	// Initialize global index to index in genesis state
+	// 인수로 들어온 binary genesis state를 JSON으로 언마샬링
 	cdc.MustUnmarshalJSON(gs, &genState)
 
+	// genesis.go의 InitGenesis() 실행
+	// 체인 asset keeper 구조체 생성 
 	InitGenesis(ctx, am.keeper, genState)
 }
 
 // ExportGenesis returns the assets module's exported genesis state as raw JSON bytes.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	// genesis.go의 ExportGenesis 실행
+	// genesis를 JSON으로 마샬링
 	genState := ExportGenesis(ctx, am.keeper)
+	// JSON을 binary로 마샬링
 	return cdc.MustMarshalJSON(genState)
 }
 
